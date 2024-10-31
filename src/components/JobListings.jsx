1065acd9983b21213103919b02fe9def7b4899d8
Ahmad Ali <xevenahmad@gmail.com>
2024-11-01 00:03:10 +0500

    add json server

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,14 +1,27 @@
 /*eslint-disable*/
+import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
-import { useState } from 'react';
 import { FaMapMarker } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
-import { jobs } from '../jobs.json';
+// import { jobs } from '../jobs.json';
 import JobListing from './JobListing';
 
 const JobListings = ({ isHome = false }) => {
-  console.log('jobs:::::::::::', jobs);
-  const recentJobs = isHome ? jobs : jobs.slice(0, 3);
+  // console.log('jobs:::::::::::', jobs);
+  // const recentJobs = isHome ? jobs : jobs.slice(0, 3);
+  const [jobs, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('http://localhost:8800/jobs')
+      .then((response) => response.json())
+      .then((data) => {
+        setJobs(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }, []);
   return (
     <section className='bg-blue-50 px-4 py-10'>
       <div className='container-xl lg:container m-auto'>
@@ -16,7 +29,7 @@ const JobListings = ({ isHome = false }) => {
           {isHome ? 'Browse Jobs' : 'Recent Jobs'}
         </h2>
         <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
-          {recentJobs.map((job) => (
+          {jobs.map((job) => (
             <JobListing key={job.id} job={job} />
           ))}
         </div>
